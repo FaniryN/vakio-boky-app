b526e1ed249e10591cf541fbc07023933bc4cb20
Faniry <fanirynomena11@gmail.com>
2025-12-13 00:11:58 +0300

    fix: SPA routing for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { fileURLToPath, URL } from 'node:url';
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
-
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +22,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // AJOUTE ÇA :
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
